cc94c4bae4362a66cd23187276bded77d7db7e7d
Stelios Vitalis <steliosvitalis@gmail.com>
2021-05-06 12:59:02 +0200

    Fix small issue with shader and fog

--- src/parsers/CityJSONWorkerParser.js
+++ src/parsers/CityJSONWorkerParser.js
@@ -108,6 +108,7 @@ function createObjectColorShader( shader, objectColors ) {
 		).replace(
 			/#include <fog_vertex>/,
 			`
+			#include <fog_vertex>
 			if ( abs ( geometryid - showGeometry ) > 0.5 && showGeometry >= 0.0 ) {
 				gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
 			}
