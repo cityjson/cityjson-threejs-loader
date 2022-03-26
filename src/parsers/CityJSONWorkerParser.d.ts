344e92a4012dad71548440d876b67236eefb3b6c
Stelios Vitalis <steliosvitalis@gmail.com>
2022-03-26 11:37:25 +0100

    Document more things in CityJSONWorkerParser

--- src/parsers/CityJSONWorkerParser.d.ts
+++ src/parsers/CityJSONWorkerParser.d.ts
@@ -2,10 +2,40 @@ import { Group, Matrix4 } from "three";
 
 export class CityJSONWorkerParser {
 
+    /**
+     * Matrix that transforms object in 3D. This is used to move big coordinates
+     * close to the origin and maintain relative position between different
+     * citymodels.
+     */
     matrix: Matrix4;
 
+    /**
+     * The size of chunks of city objects that are parsed as one mesh. Every
+     * time a chunk is finished parsing, the respective mesh is added to the
+     * scene.
+     */
+    chunkSize: Number;
+
+    /**
+     * Shows the state of loading, i.e. if there are more chunks to be parsed.
+     */
+    loading: Boolean;
+
+    /**
+     * The lookup of LoDs for this parser. It can be used to identify the LoD
+     * of a vertex based on its `lodid` attributes. This variable will be filled
+     * as the parsing proceeds with as many LoDs are occured in a file.
+     */
+    lods: Number[];
+
+    /**
+     * Callback for when a chunk is finished loading
+     */
     onChunkLoad : () => void;
     
+    /**
+     * Parses a CityJSON file (`data`) and adds it to the `scene`.
+     */
     parse( data : Object, scene : Group ) : void;
 
 }
