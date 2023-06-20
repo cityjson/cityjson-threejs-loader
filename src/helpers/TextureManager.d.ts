415c07dc3f5bf62511f39c2585e188cdc24aa8a8
imtiazShakil <imtiazdev1@gmail.com>
2023-06-20 21:42:15 +0600

    fixed formatting

--- src/helpers/TextureManager.d.ts
+++ src/helpers/TextureManager.d.ts
@@ -2,6 +2,6 @@ import { CityObjectsMaterial } from "../materials/CityObjectsMaterial";
 
 export class TextureManager {
 	constructor(citymodel: any);
-    getMaterials( baseMaterial: any ): CityObjectsMaterial[];
-    setTextureFromFile( file: any ): void;
+	getMaterials( baseMaterial: any ): CityObjectsMaterial[];
+	setTextureFromFile( file: any ): void;
 }
