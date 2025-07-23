6f8f5480c5b2748c4c28b970c1b156f8939f55d5
HideBa <baba.papa1120.ba@gmail.com>
2025-07-23 20:45:12 +0200

    wip: impl flatcitybuf loader

--- src/base/FlatCityBufLoader.js
+++ src/base/FlatCityBufLoader.js
@@ -1,4 +1,10 @@
-import { Group, Matrix4, Box3, Vector3 } from 'three';
+import { Vector3 } from 'three';
+import {
+	BufferAttribute,
+	BufferGeometry,
+	Group,
+	Matrix4
+} from 'three';
 import { CityJSONWorkerParser } from '../parsers/CityJSONWorkerParser';
 import init, { cjseqToCj } from '@cityjson/cjseq';
 export class FlatCityBufLoader {
@@ -12,7 +18,7 @@ export class FlatCityBufLoader {
 		this.parser = parser || new CityJSONWorkerParser();
 		this.httpReader = null;
 		this.fcbUrl = null;
-		this.maxFeatures = 1000;
+		this.maxFeatures = 100;
 		this.metadata = null;
 		this.header = null;
 		this.isInitialized = false;
@@ -70,17 +76,39 @@ export class FlatCityBufLoader {
 		this.fcbUrl = url;
 		await this.initWasm();
 
+	}
+
+
+
+	async load(bbox = null) {
+
+		let dataExtent = bbox;
 		try {
 
 			// Initialize HTTP reader for the FlatCityBuf file
-			this.httpReader = await new this._wasmModule.HttpFcbReader(url);
+			this.httpReader = await new this._wasmModule.HttpFcbReader(this.fcbUrl);
+
 
 			// Get header and metadata directly from reader
-			this.header = await this.httpReader.cityjson();
+			this.header = FlatCityBufLoader.mapToJson(await this.httpReader.cityjson());
 			this.metadata = await this.httpReader.meta();
-			this.isInitialized = true;
+			if (!this.isInitialized) {
 
-			console.log('FlatCityBuf URL set and metadata loaded:', url);
+				this.isInitialized = true;
+
+				// NOTE: Because there is not way to guess the first location of the data, we use the center of the geographical extent as the first location and a buffer of 500 meters
+				const ge = this.header.metadata.geographicalExtent;
+				const center = [ge[0] + (ge[3] - ge[0]) / 2, ge[1] + (ge[4] - ge[1]) / 2];
+				const bbox = {
+					minX: center[0] - 500,
+					minY: center[1] - 500,
+					maxX: center[0] + 500,
+					maxY: center[1] + 500
+				};
+
+				dataExtent = { minX: bbox.minX, minY: bbox.minY, maxX: bbox.maxX, maxY: bbox.maxY };
+
+			}
 
 
 
@@ -91,39 +119,43 @@ export class FlatCityBufLoader {
 
 		}
 
-	}
-
-
-
-	async loadBbox(minX, minY, maxX, maxY) {
-
-		if (!this.isInitialized) {
-
-			throw new Error('FlatCityBuf loader not initialized. Call setUrl() first.');
-
-		}
 
 		try {
 
-			console.log('Loading FlatCityBuf data for bbox:', { minX, minY, maxX, maxY });
-
 			// Get CityJSON directly from FlatCityBuf
-			const cityJsonData = await this._fetchCityJSON(minX, minY, maxX, maxY);
-
-			console.log('Fetched CityJSON data from FlatCityBuf:', cityJsonData);
+			const cityJsonData = await this._fetchCityJSON(dataExtent.minX, dataExtent.minY, dataExtent.maxX, dataExtent.maxY);
 
 			// Clear previous scene
-			this.clear();
+			this.scene.clear();
 
-			// Use the existing parser to create Three.js objects
+			const transform = new Matrix4().identity();
+
+			if (cityJsonData["transform"] != undefined) {
+
+				const t = cityJsonData.transform.translate;
+				const s = cityJsonData.transform.scale;
+
+				transform.set(
+					s[0], 0, 0, t[0],
+					0, s[1], 0, t[1],
+					0, 0, s[2], t[2],
+					0, 0, 0, 1
+				);
+
+			}
+
 			if (this.matrix == null) {
 
 				this.computeMatrix(cityJsonData);
+
+				this.matrix = transform;
+				this.matrix.setPosition(0, 0, 0);
 
 			}
 
 			this.parser.matrix = this.matrix;
 			this.parser.parse(cityJsonData, this.scene);
+
 
 			return this.scene;
 
@@ -146,14 +178,6 @@ export class FlatCityBufLoader {
 
 		try {
 
-			// Create spatial query for bbox
-			const queryObj = {
-				type: "bbox",
-				minX: minX,
-				minY: minY,
-				maxX: maxX,
-				maxY: maxY
-			};
 
 			// Get features from iterator and convert to CityJSON
 			const features = await this._fetchFeatures(minX, minY, maxX, maxY);
@@ -165,10 +189,9 @@ export class FlatCityBufLoader {
 
 			}
 
-			const cityjson = cjseqToCj(this.header, features);
+			const cityjson = FlatCityBufLoader.mapToJson(cjseqToCj(this.header, features));
 
 
-			// Convert Maps to JSON if needed
 			return cityjson;
 
 		} catch (error) {
@@ -203,12 +226,20 @@ export class FlatCityBufLoader {
 			const iterator = await this.httpReader.select_spatial(spatialQuery);
 
 			const features = [];
-			for (let i = 0; i < this.maxFeatures; i++) {
+			while (true) {
 
 				try {
 
 					const feature = await iterator.next();
-					features.push(feature);
+					if (!feature) {
+
+						console.log('No more features');
+						break;
+
+					}
+
+					const cjFeature = FlatCityBufLoader.mapToJson(feature);
+					features.push(cjFeature);
 
 				} catch (error) {
 
@@ -217,7 +248,14 @@ export class FlatCityBufLoader {
 
 				}
 
+				if (features.length >= this.maxFeatures) {
+
+					break;
+
+				}
+
 			}
+
 
 			return features;
 
@@ -229,5 +267,81 @@ export class FlatCityBufLoader {
 		}
 
 	}
+
+	computeMatrix(data, scale = false) {
+
+		const normGeom = new BufferGeometry();
+		console.log('Data:', data);
+
+		const vertices = new Float32Array(data.vertices.flatMap(v => [v[0], v[1], v[2]]));
+		normGeom.setAttribute('position', new BufferAttribute(vertices, 3));
+
+		normGeom.computeBoundingBox();
+		this.boundingBox = normGeom.boundingBox;
+		console.log('Bounding box:', this.boundingBox);
+		const centre = new Vector3();
+
+		normGeom.boundingBox.getCenter(centre);
+		centre.setZ(0);
+
+		const s = 1;
+
+		const matrix = new Matrix4();
+		matrix.set(
+			s, 0, 0, - s * centre.x,
+			0, s, 0, - s * centre.y,
+			0, 0, s, - s * centre.z,
+			0, 0, 0, 1
+		);
+
+		this.matrix = matrix;
+
+	}
+
+	static mapToJson(item) {
+
+		// Handle Map objects
+		if (item instanceof Map) {
+
+			const obj = {};
+			item.forEach((value, key) => {
+
+				obj[key] = FlatCityBufLoader.mapToJson(value);
+
+			});
+			return obj;
+
+		}
+
+		// Handle arrays - recursively convert each item
+		if (Array.isArray(item)) {
+
+			return item.map(FlatCityBufLoader.mapToJson);
+
+		}
+
+		// Handle plain objects - recursively convert each property
+		if (item && typeof item === "object" && item.constructor === Object) {
+
+			const result = {};
+			for (const key in item) {
+
+				if (Object.prototype.hasOwnProperty.call(item, key)) {
+
+					result[key] = FlatCityBufLoader.mapToJson((item)[key]);
+
+				}
+
+			}
+
+			return result;
+
+		}
+
+		// Return primitives and other types unchanged
+		return item;
+
+	}
+
 
 }
